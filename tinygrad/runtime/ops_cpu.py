0083ab7e4dfbb5d66bd8d356d6edce69490a706e
Peter Ross <pross@xvid.org>
2023-06-03 20:18:30 +1000

    ops/cpu: prevent type confusion when CMPEQ'ing non-float tensors
    the return type of CMPEQ must match the out_dtype determination
    of tinygrad.py/elementwise_op().
    
    simple test case:
    
    a = Tensor.ones(1, dtype=dtypes.int32)
    b = Tensor.ones(1, dtype=dtypes.int64)
    c = (a == b)
    print(c.numpy())
    
    exhaustive test case:
    
    from dataclasses import asdict
    all_dtypes = asdict(dtypes()).values()
    for xt in all_dtypes:
        for yt in all_dtypes:
           x = Tensor.ones(1, dtype=xt)
           y = Tensor.ones(1, dtype=yt)
           z = x == y
           assert z.dtype == max(xt, yt)

--- tinygrad/runtime/ops_cpu.py
+++ tinygrad/runtime/ops_cpu.py
@@ -28,7 +28,7 @@ def einsum_mulacc(einsum, get_strides, expand):
 
 numpy_fxn_for_op: Dict[Op, Callable] = {**base_fxn_for_op, **{
   UnaryOps.NOOP: lambda x: np.require(x, requirements='C'), UnaryOps.EXP: np.exp, UnaryOps.LOG: np.log, UnaryOps.CAST: lambda x,y: x.astype(y.np), UnaryOps.SIN: np.sin,
-  BinaryOps.MAX: np.maximum, BinaryOps.CMPEQ: lambda x,y: (x==y).astype(np.float32),
+  BinaryOps.MAX: np.maximum, BinaryOps.CMPEQ: lambda x,y: (x==y).astype(max(dtypes.from_np(x.dtype), dtypes.from_np(y.dtype)).np),
   MovementOps.PERMUTE: lambda x, order: x.transpose(order), MovementOps.PAD: np.pad, MovementOps.EXPAND: np.broadcast_to,
   MovementOps.STRIDE: lambda x, arg: x[tuple(slice(None, None, i) for i in arg)],
   FusedOps.MULACC: einsum_mulacc(lambda s,a,b: np.einsum(s, a.copy(), b.copy()), lambda x: x.strides, np.broadcast_to),
